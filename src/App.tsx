911b0793d1ee75a434cf7787d0314534e05461e5
Menna <monnaa379@gmail.com>
2026-08-07 04:59:05 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,52 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+
+import Register from "./Components/Register/Register";
+import Login from "./Components/Login/Login";
+import ForgotPassword from "./Components/ForgotPassword/ForgotPassword";
+
 import TechniciansListPage from "./pages/TechniciansListPage/TechniciansListPage";
-import CentersListPage from "./pages/CentersListPage/CentersListPage";
 import TechnicianDetailPage from "./pages/TechnicianDetailPage/TechnicianDetailPage";
+import CentersListPage from "./pages/CentersListPage/CentersListPage";
 import CenterDetailPage from "./pages/CenterDetailPage/CenterDetailPage";
-import Register from '../src/Components/Register/Register'
-import Login from '../src/Components/Login/Login'
-import ForgotPassword from '../src/Components/ForgotPassword/ForgotPassword'
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './App.css'
- let router = createBrowserRouter([
-    {path:'register' ,element : <Register/>},
-    {path :'login' ,element :<Login/>},
-    {path :'forgot-password' ,element :<ForgotPassword/>},
-  ])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <TechniciansListPage />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/forgot-password",
+    element: <ForgotPassword />,
+  },
+  {
+    path: "/technicians",
+    element: <TechniciansListPage />,
+  },
+  {
+    path: "/technicians/:id",
+    element: <TechnicianDetailPage />,
+  },
+  {
+    path: "/centers",
+    element: <CentersListPage />,
+  },
+  {
+    path: "/centers/:id",
+    element: <CenterDetailPage />,
+  },
+]);
+
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/technicians" replace />} />
-        <Route path="/technicians" element={<TechniciansListPage />} />
-        <Route path="/technicians/:id" element={<TechnicianDetailPage />} />
-        <Route path="/centers" element={<CentersListPage />} />
-        <Route path="/centers/:id" element={<CenterDetailPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
- 
-
-  return (
-    <>  
-<RouterProvider router={router}></RouterProvider>
-
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App;
