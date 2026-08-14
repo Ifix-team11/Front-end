33dd0aa684984ec8cc48f9f28f200e9b927c7783
Menna <monnaa379@gmail.com>
2026-08-14 08:32:31 +0300

    Update TechniciansListPage.tsx

--- src/pages/TechniciansListPage/TechniciansListPage.tsx
+++ src/pages/TechniciansListPage/TechniciansListPage.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
-import Navbar from "../../components/Navbar/Navbar";
-import Footer from "../../components/Footer/Footer";
+// import Navbar from "../../components/Navbar/Navbar";
+// import Footer from "../../components/Footer/Footer";
 import FilterBar from "../../components/FilterBar/FilterBar";
 import TechnicianCard from "../../components/TechnicianCard/TechnicianCard";
 import { technicians } from "../../data/mockData";
